c7020e8010afe7bdd35ab2beb7198d04a02ae852
rodentskie <rodentskie@gmail.com>
2026-09-21 15:38:52 +0800

    feat: integrate chakra

--- apps/app/src/app/layout.tsx
+++ apps/app/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import { EmotionRegistry } from "@inventramed/snippets/emotion-registry"
 import { Provider } from "@inventramed/snippets/provider"
 
+export const metadata = {
+  title: "Test"
+}
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning>
--- apps/app/src/app/page.tsx
+++ apps/app/src/app/page.tsx
@@ -1,5 +1,11 @@
-import { Button, HStack, Code } from "@chakra-ui/react"
+import { Code, HStack } from "@chakra-ui/react"
+import { ColorModeButton } from "@inventramed/snippets/color-mode"
 
 export default function Index() {
-  return <Code>{`console.log("Hello, world!")`}</Code>
-};
+  return (
+    <HStack p="4" gap="4">
+      <Code>{`console.log("Hello, world!")`}</Code>
+      <ColorModeButton />
+    </HStack>
+  )
+}
